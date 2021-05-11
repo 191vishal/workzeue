5e45125cfec99ed5c4816ba30a386f25a9317f40
VishalBhavsar <vishalbhavsar191@gmail.com>
2021-05-11 12:12:25 +0530

    [WIP] improve

---
+++ Student/odoo_trainee/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'odoo_trainee',
+    'version': '1.0',
+    'category': 'Trainee Demo',
+    'sequence': 5,
+    'summary': 'Trainee demo module',
+    'depends': ['base'],
+
+    'description': """
+            module for trainee demo
+            """,
+            
+
+    'data' : [
+        'security/ir.model.access.csv',
+        # 'data/data.xml',
+        'wizard/student_update_wizard_view.xml',
+        'views/home.xml',
+    ],
+
+    'application' : True
+
+}
